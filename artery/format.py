b5a7d42a7782817730ed59f4b7b7cccbb4f7f19e
Naomi Most <naomi@nthmost.com>
2023-10-25 12:36:37 -0400

    new Receipt* classes to abstract chunk contents as image or text

--- artery/format.py
+++ artery/format.py
@@ -2,14 +2,17 @@ from PIL import Image, ImageDraw, ImageFont
 
 MAX_WIDTH = 384
 
-def text_to_img(text, font_path, font_size):
+def text_to_img(text, font_path, font_size, align="left", max_width=MAX_WIDTH):
     """Convert text to an image using a specific TTF font."""
     font = ImageFont.truetype(font_path, font_size)
 
     # this line doesn't work, so fudging it for now.
     #width, height = font.getsize(text)
 
-    width = MAX_WIDTH
+
+    #TODO: respect "align"
+
+    width = max_width
     height = 100
     
     # Create the actual image with the correct size
@@ -26,8 +29,8 @@ def resize_image(img_path, max_width=MAX_WIDTH):
     img = Image.open(img_path)
 
     # Resize the image if its width exceeds MAX_WIDTH
-    if img.width > MAX_WIDTH:
-        new_height = int(MAX_WIDTH * img.height / img.width)
-        img = img.resize((MAX_WIDTH, new_height))
+    if img.width > max_width:
+        new_height = int(max_width * img.height / img.width)
+        img = img.resize((max_width, new_height))
 
     return img
--- test_integration.py
+++ test_integration.py
@@ -9,19 +9,13 @@ SAMPLE_EXPERIENCE = "This is a sample experience text to be printed on the recei
 def print_receipt(receipt):
     printer = ArteryPrinter()
     
-    for line in receipt.receipt:
-        if "[IMAGE]" in line and "[/IMAGE]" in line:
-            img_path = line.split("[IMAGE]")[1].split("[/IMAGE]")[0]
-            printer.print_image(img_path)
-        elif "[FONT " in line and "[/FONT]" in line:
-            font_info = line.split("[FONT ")[1].split("]")[0]
-            # Process the font_info here if needed, e.g., to extract size, boldness, etc.
-            text = line.split("[/FONT]")[1]
-            # Adjust the below print_text function as needed
-            printer.print_text(text, font_size='medium')
-        else:
-            printer.print_text(line)
-    
+    for obj in receipt.receipt:
+        if obj.__name__ == "ReceiptImage":
+            printer.print_image(obj.filepath)
+
+        elif obj.__name__ == "ReceiptText":
+            printer.print_text(text, font_size=obj.font_size, bold=obj.bold, underline=obj.underline)    
+
     printer.finish()
 
 # Example of how to use the ExperienceReceipt class and the print_receipt function
