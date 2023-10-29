cb1fdea0e5b9e3c0fdf1efc6daf7a87ec7c81852
Naomi Most <naomi@nthmost.com>
2023-10-29 01:08:11 -0700

    cruft removal

--- artery/format.py
+++ artery/format.py
@@ -27,26 +27,6 @@ def text_to_img(text, font_path, font_size, align="left", max_width=MAX_WIDTH):
     return img
 
 
-
-def old_text_to_img(text, font_path, font_size, align="left", max_width=MAX_WIDTH):
-    """Convert text to an image using a specific TTF font."""
-    font = ImageFont.truetype(font_path, font_size)
-
-    # this line doesn't work, so fudging it for now.
-    #width, height = font.getsize(text)
-
-    #TODO: respect "align"
-
-    width = max_width
-    height = 100
-    
-    # Create the actual image with the correct size
-    img = Image.new('L', (width, height), color=255)
-    d = ImageDraw.Draw(img)
-    d.text((0, 0), text, font=font, fill=0)
-    return img
-
-
 def resize_image(img_path, max_width=MAX_WIDTH):
     """Returns a PNG image loaded from img_path, resizing if necessary."""
 
